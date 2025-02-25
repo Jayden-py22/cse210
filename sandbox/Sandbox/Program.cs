f8d42ae102c4b4a90a54c29f30d29822bc087417
Jayden-py22 <haij0428@gmail.com>
2025-02-25 09:49:37 -0700

    Added Input Line

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Hello CSE 210");
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine($"{name}");
     }
 }
