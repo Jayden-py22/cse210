4ddacff1e331e99f88e8844de3d88db4ed8f66e3
Jayden-py22 <haij0428@gmail.com>
2025-02-26 09:34:42 -0700

    Updated 2/26

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.Write("Please enter your first name: ");
+        string firstName = Console.ReadLine();
+        Console.WriteLine($"{firstName}");
     }
 }
