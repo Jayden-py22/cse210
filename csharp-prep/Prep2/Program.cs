12d9c57464532df286575078f9cb0d7cdba1b71a
Jayden-py22 <haij0428@gmail.com>
2025-03-05 18:12:36 -0700

    Updated on 2/27

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        if gradescale >= 90 && >= 80 
+        Console.WriteLine("You have A lever grade.")
+        else if grade scale 
     }
 }
